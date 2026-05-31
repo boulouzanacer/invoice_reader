617ff0a24a2014355dff10ca031b61e54c20fce1
BOULOUZA <boulouza.nacer@gmail.com>
2026-05-31 15:15:45 +0100

    Add proper Laravel .gitignore file

--- storage/framework/views/26bf61240e5a49743579944926f9378a999aa662.php
+++
@@ -1,5 +0,0 @@
-<div class="flex flex-col items-center">
-    <img src="<?php echo e(asset('images/logo.png')); ?>" alt="<?php echo e(config('app.name')); ?>" onerror="this.style.display='none'; this.nextElementSibling.style.display='block'" <?php echo e($attributes); ?>>
-    <span class="hidden font-bold text-blue-600 text-xl"><?php echo e(config('app.name')); ?></span>
-</div>
-<?php /**PATH D:\xampp\htdocs\invoice_reader\resources\views/components/application-logo.blade.php ENDPATH**/ ?>
